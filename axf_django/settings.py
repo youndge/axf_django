27f3e4699b384712390e73115a09886529cf8c95
youndge <18610457408@163.com>
2018-07-14 20:47:28 +0800

    Set up database and template directory

--- axf_django/settings.py
+++ axf_django/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'axf_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'axf_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'axf',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'POST':'3306',
     }
 }
 

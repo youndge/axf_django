d3b292441a107f938ac131e9176f2cbdc256163e
Youndge <18610457408@163.com>
2018-07-15 22:16:10 +0800

    Set up urls and views,build up the folders structure;

---
+++ axf_app/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: Youndge
+# Instructions:
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('^home/$', views.home, name = 'home'),
+    path('^market/$', views.market, name = 'market'),
+    path('cart/$', views.cart, name = 'cart'),
+    path('^mine/$', views.mine, name = 'mine'),
+]
--- axf_app/views.py
+++ axf_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return  render(request, 'axf/home.html')
+
+def market(request):
+    return  render(request, 'axf/market.html')
+
+def cart(request):
+    return  render(request, 'axf/cart.html')
+
+def mine(request):
+    return  render(request, 'axf/mine.html')
